ab92623af93c1215d92ebccda7c28d200e27f493
ravishansenevirathna <travishan55@gmail.com>
2024-10-24 19:56:46 +0530

    add deployment files

--- order/src/main/java/com/codedecode/order/controller/OrderController.java
+++ order/src/main/java/com/codedecode/order/controller/OrderController.java
@@ -1,10 +1,12 @@
 package com.codedecode.order.controller;
 
+import com.codedecode.order.dto.OrderDTO;
+import com.codedecode.order.dto.OrderDTOFromFE;
 import com.codedecode.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
@@ -12,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
     @Autowired
     OrderService orderService;
+
+    @PostMapping("/saveOrder")
+    public ResponseEntity<OrderDTO> saveOrder(@RequestBody OrderDTOFromFE orderDetails){
+        OrderDTO orderSavedInDB = orderService.saveOrderInDb(orderDetails);
+        return new ResponseEntity<>(orderSavedInDB, HttpStatus.CREATED);
+    }
 }
